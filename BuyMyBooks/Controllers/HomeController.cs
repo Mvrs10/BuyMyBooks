6d26bfb3c4b426024b0f90c5b7459df7250a2e3a
Minh Khoi Phan <pmka42@gmail.com>
2026-05-11 13:39:15 -0400

    NuGet EFCore.SQLServer and Tools.

--- BuyMyBooks/Controllers/HomeController.cs
+++ BuyMyBooks/Controllers/HomeController.cs
@@ -1,5 +1,4 @@
 using System.Diagnostics;
-using BuyMyBooks.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BuyMyBooks.Controllers
@@ -21,12 +20,6 @@ namespace BuyMyBooks.Controllers
         public IActionResult Privacy()
         {
             return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
---
+++ BuyMyBooks/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace BuyMyBooks.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+
+    }
+}
