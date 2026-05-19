3893839d3802637a5ee9acbe917586a20b66f1cf
Minh Khoi Phan <pmka42@gmail.com>
2026-05-19 12:04:20 -0400

    Migraged into CategoryService.

---
+++ BuyMyBooks.Business/Services/CategoryService.cs
@@ -0,0 +1,60 @@
+﻿using BuyMyBooks.Business.Services.IServices;
+using BuyMyBooks.DataAccess.Data;
+using BuyMyBooks.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuyMyBooks.Business.Services;
+
+public class CategoryService : ICategoryService
+{
+    private readonly ApplicationDbContext _context;
+
+    public CategoryService(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<Category>> GetAllCategoriesAsync()
+    {
+        List<Category> categories = await _context.Categories.ToListAsync();
+
+        return categories;
+    }
+
+    public async Task<Category?> GetCategoryByIdAsync(int id)
+    {
+        Category? category = await _context.Categories.FindAsync(id);
+        return category;
+    }
+
+    public async Task<Category> CreateCategoryAsync(Category category)
+    {
+        _context.Categories.Add(category);
+        await _context.SaveChangesAsync();
+        return category;
+    }
+
+    public async Task UpdateCategoryAsync(Category category)
+    {
+        _context.Categories.Update(category);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteCategoryAsync(int id)
+    {
+        Category? category = await _context.Categories.FindAsync(id);
+
+        if (category == null)
+        {
+            throw new KeyNotFoundException($"Category {id} not found!");
+        }
+
+        _context.Categories.Remove(category);
+        await _context.SaveChangesAsync();
+    }
+}
---
+++ BuyMyBooks.Business/Services/IServices/ICategoryService.cs
@@ -0,0 +1,22 @@
+﻿using BuyMyBooks.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuyMyBooks.Business.Services.IServices;
+
+public interface ICategoryService
+{
+    Task<Category?> GetCategoryByIdAsync(int id);
+
+    Task<List<Category>> GetAllCategoriesAsync();
+
+    Task<Category> CreateCategoryAsync(Category category);
+
+    Task UpdateCategoryAsync(Category category);
+
+    Task DeleteCategoryAsync(int id);
+}
+
